8c8e78461809ec968756a8c90133c481fc980dca
Chanky <50941944+LinelessChanky@users.noreply.github.com>
2022-03-23 18:32:40 +0100

    Prime classi

---
+++ src/com/Locality.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Locality {
+    String name;
+    int danger, lucky;
+
+    LocalType locType;
+    ArrayList<Action> actions;
+
+    public Locality(String name, int danger, int lucky) {
+        this.name = name;
+        this.danger = danger;
+        this.lucky = lucky;
+    }
+
+    public String getAtcions(){
+
+    }
+
+    public Item Actiop(){
+
+    }
+}
---
+++ src/com/LocalityGenerator.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.random.RandomGenerator;
+
+public class LocalityGenerator {
+    private static LocalityGenerator singleInstance = null;
+
+    String namePool[] ={"Casa ","Locanda ","Chiesa ","Gilda ","Villa ","Grotta ","","",""};
+    String aggettivi[] ={"Casa ","Locanda ","Chiesa ","Gilda ","Villa ","Grotta ","","",""};
+
+    private Locality generateLocality(){
+        String name;
+        int ind, indAgg, lucky, danger;
+        ind = (int)(Math.random()*((20-1)+1)+1);
+        if(ind > 10) {
+            indAgg = (int) (Math.random() * ((20 - 10) + 1) + 10);
+            name = namePool[ind] + aggettivi[(int) (indAgg)];
+        }else{
+            indAgg = (int) (Math.random() * ((10 - 0) + 1) + 0);
+            name = namePool[ind] + aggettivi[(int) (indAgg)];
+        }
+        if(indAgg>=5 && indAgg<10 || indAgg>=15 && indAgg<20){
+            lucky=(int) (Math.random() * ((5 - 1) + 1) + 1);
+            danger=(int) (Math.random() * ((10 - 5) + 1) + 5);
+        }else{
+            lucky=(int) (Math.random() * ((10 - 5) + 1) + 5);
+            danger=(int) (Math.random() * ((5 - 1) + 1) + 1);
+        }
+        //return Locality(name,danger,lucky);
+    }
+
+    public static LocalityGenerator getInstance() {
+        if (singleInstance == null)
+            singleInstance = new LocalityGenerator();
+        return singleInstance;
+    }
+}
